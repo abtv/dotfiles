85fffba1ebac35b0849e58c383a1252ecf1a665a
Andrey Butov <andreybutov@mail.ru>
2016-08-11 11:30:15 +0300

    log information while setup

--- install.sh
+++ install.sh
@@ -1,39 +1,47 @@
+echo 'Updating packages...'
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update -y
+echo 'Packages were updated\n\n\n'
 
+echo 'Installing packages...'
 sudo apt-get install -y language-pack-ru
 sudo apt-get install -y htop
 sudo apt-get install -y zip
 sudo apt-get install -y unzip
 sudo apt-get install -y curl
 sudo apt-get install -y git
+echo 'Packages were installed\n\n\n'
 
-#Java
+echo 'Installing Java...'
 echo debconf shared/accepted-oracle-license-v1-1 select true | \
   sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | \
   sudo debconf-set-selections
 sudo apt-get install -y oracle-java8-installer
+echo 'Java was installed\n\n\n'
 
-#Postgres
+echo 'Installing Postgres...'
 sudo apt-get install -y postgresql
 su - postgres -c "psql -U postgres -d postgres -c \"alter user postgres with password 'postgres';\""
+echo 'Postgres was installed\n\n\n'
 
-#Leiningen
+echo 'Installing Leiningen'
 mkdir bin
 cd bin
 wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
 sudo chmod u+x lein
 cd ~
+echo 'Leiningen was installed\n\n\n'
 
-#Nodejs
+echo 'Installing Nodejs...'
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install -g tern
 sudo npm install -g js-beautify
 sudo npm install -g jshint
+echo 'Nodejs was installed\n\n\n'
 
-#Emacs
+echo 'Installing Emacs...'
 sudo apt-get install -y emacs
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 git clone https://github.com/abtv/dotfiles.git
@@ -41,21 +49,26 @@ cp dotfiles/.spacemacs ~/
 rm -rf ./dotfiles
 echo 'export TERM=xterm-256color' >> ~/.zshrc
 echo 'alias emacs="emacs -nw"' >> ~/.zshrc
+echo 'Emacs was installed\n\n\n'
 
-#Oh my zsh
+echo 'Installing Oh my zsh'
 sudo apt-get install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -y
+echo 'Oh my zsh was installed\n\n\n'
 
-#ElasticSearch
+echo 'Installing ElasticSearch...'
 wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.3.5/elasticsearch-2.3.5.deb
 sudo dpkg -i elasticsearch-2.3.5.deb
 rm elasticsearch-2.3.5.deb
 sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable elasticsearch.service
 sudo /bin/systemctl start elasticsearch.service
+echo 'ElasticSearch was installed\n\n\n'
 
-#ufw
+echo 'Configuring UFW...'
 sudo ufw default deny incoming
 sudo ufw default allow outcoming
 sudo ufw allow ssh
 sudo ufw enable -y
+echo 'UFW was configured'
+echo 'Setup is done!'
