7bf2c4d18028a9ba470762275ae88803702c15d7
abtv <andreybutov83@gmail.com>
2020-02-15 19:34:08 +0300

    Add karabiner.json config

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ git config --global core.editor /usr/local/bin/nvim
 ln -s /Users/andrew/dotfiles/alacritty/alacritty.yml /Users/andrew/.config/alacritty/alacritty.yml
 ln -s /Users/andrew/dotfiles/vim/init.vim /Users/andrew/.config/nvim/init.vim
 ln -s /Users/andrew/dotfiles/.zshrc /Users/andrew/.zshrc
+cp /Users/andrew/dotfiles/karabiner/karabiner.json /Users/andrew/.config/karabiner/karabiner.json
