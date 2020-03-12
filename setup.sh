652a2dd4a3c96ecd31fff048cf666d3b52dc050f
abtv <andreybutov83@gmail.com>
2020-03-12 13:44:56 +0300

    Link CoC settings file to dotfiles folder

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 git config --global core.editor /usr/local/bin/nvim
 ln -s /Users/andrew/dotfiles/alacritty/alacritty.yml /Users/andrew/.config/alacritty/alacritty.yml
 ln -s /Users/andrew/dotfiles/vim/init.vim /Users/andrew/.config/nvim/init.vim
+ln -s /Users/andrew/dotfiles/vim/coc-settings.json /Users/andrew/.config/nvim/coc-settings.json
 ln -s /Users/andrew/dotfiles/.zshrc /Users/andrew/.zshrc
 ln -s /Users/andrew/dotfiles/.editorconfig /Users/andrew/.editorconfig
 cp /Users/andrew/dotfiles/karabiner/karabiner.json /Users/andrew/.config/karabiner/karabiner.json
