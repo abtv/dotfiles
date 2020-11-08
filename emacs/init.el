eed5028c3b6c6bd22147624d4ac35cbc9781b3a3
abtv <andreybutov83@gmail.com>
2020-11-08 21:15:26 +0300

    Add Emacs project settings

---
+++ emacs/init.el
@@ -0,0 +1,7 @@
+(projectile-add-known-project "~/work/backend")
+(projectile-add-known-project "~/work/portal-spa")
+(projectile-add-known-project "~/work/signup")
+(projectile-add-known-project "~/work/salesforce")
+(projectile-add-known-project "~/work/office")
+(projectile-add-known-project "~/work/scraper")
+
