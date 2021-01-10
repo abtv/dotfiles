cea1abb582051a6963c0cad845a15f63ad9387a1
abtv <andreybutov83@gmail.com>
2021-01-10 23:09:33 +0300

    Emacs: show tabs for buffers

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -43,6 +43,24 @@
  '(vterm-color-magenta ((t (:foreground "#a626a4" :background "#cccccc" :inherit nil)))))
 
 
+; Set centaur-tabs UI
+(custom-set-faces
+ '(centaur-tabs-selected  ((t (:background "#dddddd" :inherit nil)))))
+(custom-set-faces
+ '(centaur-tabs-selected-modified ((t (:background "#dddddd" :inherit nil)))))
+(custom-set-faces
+ '(centaur-tabs-unselected ((t (:background "#cccccc" :inherit nil)))))
+(custom-set-faces
+ '(centaur-tabs-unselected-modified ((t (:background "#cccccc" :inherit nil)))))
+(custom-set-faces
+ '(centaur-tabs-active-bar-face ((t (:foreground "#cccccc" :background "#cccccc" :inherit nil)))))
+(setq centaur-tabs-set-bar 'under)
+(setq centaur-tabs-set-icons nil)
+(setq centaur-tabs-set-close-button nil)
+(setq centaur-tabs-adjust-buffer-order 'right)
+(add-hook 'centaur-tabs-init-hook #'centaur-tabs-change-fonts-after-init)
+
+
 ; Customize Git & Magit
 (require 'magit)
 (setq doom-modeline-vcs-max-length 32)
@@ -71,13 +89,13 @@
 
 ; Key bindings (faster to type)
 (map! "C-k" #'switch-to-next-buffer)
-(map! :map magit-mode-map :n "C-k" #'switch-to-next-buffer)
-(map! :map vterm-mode-map :n "C-k" #'switch-to-next-buffer)
-(map! :map org-mode-map :n "C-k" #'switch-to-next-buffer)
+(map! :map magit-mode-map :n "C-k" #'centaur-tabs-forward)
+(map! :map vterm-mode-map :n "C-k" #'centaur-tabs-forward)
+(map! :map org-mode-map :n "C-k" #'centaur-tabs-forward)
 (map! "C-j" #'switch-to-prev-buffer)
-(map! :map magit-mode-map :n "C-j" #'switch-to-prev-buffer)
-(map! :map vterm-mode-map :n "C-j" #'switch-to-prev-buffer)
-(map! :map org-mode-map :n "C-j" #'switch-to-prev-buffer)
+(map! :map magit-mode-map :n "C-j" #'centaur-tabs-backward)
+(map! :map vterm-mode-map :n "C-j" #'centaur-tabs-backward)
+(map! :map org-mode-map :n "C-j" #'centaur-tabs-backward)
 
 
 ; Key bindings (slower to type)
@@ -152,6 +170,9 @@
 
 
 ; Functions for customizations
+(defun centaur-tabs-change-fonts-after-init ()
+  ""
+  (centaur-tabs-change-fonts "JetBrains Mono" 150))
 
 (defun magit-on-display-buffer ()
   ""
