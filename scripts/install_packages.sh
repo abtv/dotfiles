72037548bae18197da77cf3688e7b64b1b8f1f93
abtv <andreybutov83@gmail.com>
2021-05-01 14:13:43 +0300

    Installers: remove unused packages

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,37 +1,32 @@
 brew install tmux
 brew install nvim
 brew install emacs
-brew install visual-studio-code
 brew install ag
 brew install tig
 brew install htop
 brew install tree
 brew install node@12
-brew install postgresql@9.6
-brew install openjdk@11
 brew install flyway
-brew install go
-brew install xmlstarlet
 brew install awscli
 brew install ansible
 brew install flyway
+
 brew cask install karabiner-elements
-brew cask install alacritty
-brew cask install visual-studio-code
 brew cask install firefox
-brew cask install google-chrome
+brew cask install alacritty
 brew cask install slack
 brew cask install postico
-brew cask install sublime-text
 brew cask install workflowy
-brew cask install notion
 brew cask install telegram
+brew cask install notion
+brew cask install 1password
+brew cask install vlc
+
+brew cask install docker
+
 brew cask install skype
 brew cask install microsoft-teams
-brew cask install docker
-brew cask install turbo-boost-switcher
-brew cask install 1password
-brew cask install postman
 brew cask install sfdx
-brew cask install vlc
-brew cask install intel-power-gadget
+
+brew cask install turbo-boost-switcher
+brew cask install google-chrome
