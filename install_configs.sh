043b9571ef20d508399dd2bc37fdc2663d6c4551
abtv <andreybutov83@gmail.com>
2021-05-04 23:03:15 +0300

    Emacs: rename .doom.d folder into emacs; refactor install_configs.sh file

--- install_configs.sh
+++ install_configs.sh
@@ -1,20 +1,20 @@
-# Git
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+# Karabiner
+mkdir -p ~/.config/karabiner
+ln -sf ~/dotfiles/karabiner.json ~/.config/karabiner/karabiner.json
 
 # Alacritty
 mkdir -p ~/.config/alacritty
 ln -sf ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 
-# ZSH
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-
 # Tmux
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
-# Karabiner
-mkdir -p ~/.config/karabiner
-ln -sf ~/dotfiles/karabiner.json ~/.config/karabiner/karabiner.json
+# ZSH
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+# Git
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 
 # Editor config
 ln -sf ~/dotfiles/.editorconfig ~/.editorconfig
@@ -26,3 +26,9 @@ ln -sf ~/dotfiles/vim/coc-settings.json ~/.config/nvim/coc-settings.json
 
 # Vim as a Git editor
 git config --global core.editor /usr/local/bin/nvim
+
+# Emacs
+mkdir -p ~/.doom.d
+ln -sf ~/dotfiles/emacs/config.el ~/.doom.d/config.el
+ln -sf ~/dotfiles/emacs/init.el ~/.doom.d/init.el
+ln -sf ~/dotfiles/emacs/packages.el ~/.doom.d/packages.el
