a6a6f6119b24d0f8e6b6e927fab1d7d85f29b401
abtv <andreybutov83@gmail.com>
2022-02-09 23:06:05 +0300

    System: remove non-necessary packages

--- install_packages.sh
+++ install_packages.sh
@@ -5,26 +5,12 @@ brew install tig
 brew install htop
 brew install tree
 brew install node@16
-brew install openjdk
-brew install flyway
 brew install awscli
 brew install ansible
-brew install pdftk-java
 
 brew cask install karabiner-elements
-brew cask install firefox
 brew cask install alacritty
-brew cask install slack
 brew cask install postico
-brew cask install workflowy
-brew cask install telegram
-brew cask install notion
-brew cask install 1password
-brew cask install vlc
-brew cask install dropbox
-
 brew cask install docker
-brew tap hashicorp/tap
-brew install hashicorp/tap/terraform
 
-brew cask install google-chrome
+brew cask install workflowy
