31b91cb0fdad9dec01ffecbcce1066d61536bc16
abtv <andreybutov83@gmail.com>
2022-05-14 23:58:02 +0300

    System: add font smoothing script for old displays

---
+++ font_smoothing.sh
@@ -0,0 +1,7 @@
+# Apply it only for old displays!
+# Enable heavy font smoothing for old displays (requires MacOS restart)
+defaults -currentHost write -g AppleFontSmoothing -int 3
+
+# I took it from https://www.fontsmoothingadjuster.com
+# Reset to default:
+# defaults -currentHost delete -g AppleFontSmoothing
