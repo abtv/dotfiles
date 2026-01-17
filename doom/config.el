6accd60d4cadf80ba6f6e4389cd3e0a27d783141
abtv <andreybutov83@gmail.com>
2026-01-17 21:12:38 +0100

    Emacs: disable LSP, use tree-sitter only

--- doom/config.el
+++ doom/config.el
@@ -1,28 +1,20 @@
-(setq frame-title-format "Emacs")
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-(add-to-list 'initial-frame-alist '(width . 120))
-(add-to-list 'initial-frame-alist '(height . 40))
-(setq doom-theme 'doom-homage-white) ; tango doom-zenburn doom-acario-light
-(setq doom-font (font-spec :family "JetBrains Mono" :size 15))
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+(setq frame-title-format "Emacs")
+(setq doom-theme 'modus-operandi-tritanopia)
+(add-hook 'window-setup-hook (lambda ()
+                               (set-background-color "#f0f0f0")))
+
 (setq display-line-numbers-type nil)
 (setq confirm-kill-emacs nil)
-
-(require 'recentf)
-(add-to-list 'recentf-exclude
-             (recentf-expand-file-name "~/.config/emacs/.local/etc/workspaces/autosave"))
-
 (setq org-directory "~/org/")
-(setq org-ellipsis "▼")
-(require 'org-download)
-(add-hook 'org-mode-hook 'org-download-enable)
-(setq-default org-download-heading-lvl nil)
-(setq-default org-download-image-dir "~/org/images")
-(setq org-image-actual-width 1200)
-(setq org-agenda-span 30)
 
-;; Zoom in and zoom out for Org mode
-(map! :after org :map org-mode-map "s-." 'org-narrow-to-subtree)
-(map! :after org :map org-mode-map "s-," 'widen)
+(add-to-list 'default-frame-alist '(width  . 120)) ; character columns
+(add-to-list 'default-frame-alist '(height . 50))  ; lines of text
+(set-face-attribute 'default nil :height 150)
 
 ;; Setup J and K keys
 (defun previous-lines ()
@@ -34,62 +26,5 @@
 (evil-global-set-key 'normal "J" 'next-lines)
 (evil-global-set-key 'normal "K" 'previous-lines)
 
-;; Global keys
-(global-set-key (kbd "s-i") '+vterm/toggle)
-(global-set-key (kbd "s-f") '+default/search-project)
-(global-set-key (kbd "s-;") 'projectile-find-file)
 (global-set-key (kbd "C-l") 'doom/toggle-line-numbers)
 (map! :desc "Delete current buffer" :leader "d" 'kill-current-buffer)
-
-;; Make vterm bigger
-(after! vterm
-  (set-popup-rule! "*doom:vterm-popup:main" :size 0.75 :vslot -4 :select t :quit nil :ttl 0 :side 'bottom))
-
-;; Toggle big/small screen size (I use small mode to take notes during meetings and big mode for focused work)
-(defvar centered-frame t)
-
-(defun set-width (width)
-  (set-frame-width (selected-frame) width))
-
-(defun set-centered-frame ()
-  (set-width 120)
-  (if (<= (display-pixel-width) 1440)
-      (set-frame-position (selected-frame) 200 50)
-    (if (<= (display-pixel-width) 1728)
-        (set-frame-position (selected-frame) 320 150)
-      (if (<= (display-pixel-width) 1920)
-          (set-frame-position (selected-frame) 400 180)
-        (set-frame-position (selected-frame) 400 180) ;; update it later if I have a bigger screen
-        ))))
-
-(defun set-right-side-frame ()
-  (set-width 50)
-  (if (<= (display-pixel-width) 1440)
-      (set-frame-position (selected-frame) 970 0)
-    (if (<= (display-pixel-width) 1728)
-        (set-frame-position (selected-frame) 1250 0)
-      (if (<= (display-pixel-width) 1920)
-          (set-frame-position (selected-frame) 1450 0)
-        (set-frame-position (selected-frame) 1450 0) ;; update it later if I have a bigger screen
-        ))))
-
-(defun toggle-frame-size ()
-  (interactive)
-  (if (eq centered-frame 'nil)
-      (set-centered-frame)
-    (set-right-side-frame))
-  (setq centered-frame (not centered-frame)))
-
-(map! :desc "Toggle frame size" :leader "v" 'toggle-frame-size)
-;; End of toggle big/small screen size
-
-;; Open journal on startup
-(add-hook 'window-setup-hook (lambda () (find-file "~/org/journal.org")))
-
-(map! :desc "Quick commit" :leader "g s" (lambda ()
-                                           (interactive)
-                                           (shell-command "git add .;git commit -a -m 'Update'")))
-
-(add-hook 'window-setup-hook (lambda ()
-                               (set-centered-frame)
-                               (set-background-color "#d0d0d0")))
--- doom/init.el
+++ doom/init.el
@@ -1,3 +1,4 @@
+
 ;;; init.el -*- lexical-binding: t; -*-
 
 ;; This file controls what Doom modules are enabled and what order they load
@@ -22,7 +23,7 @@
 
        :completion
        ;;company           ; the ultimate code completion backend
-       ;;(corfu +orderless)  ; complete with cap(f), cape and a flying feather!
+       (corfu +orderless)  ; complete with cap(f), cape and a flying feather!
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
@@ -35,7 +36,6 @@
        ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;(emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       ;;hydra
        ;;indent-guides     ; highlighted indent columns
        ;;ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
@@ -44,10 +44,11 @@
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
+       ;;smooth-scroll     ; So smooth you won't believe it's not butter
        ;;tabs              ; a tab bar for Emacs
        ;;treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
-       ;;(vc-gutter +pretty) ; vcs diff in the fringe
+       (vc-gutter +pretty) ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
@@ -57,7 +58,7 @@
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       (format +onsave)  ; automated prettiness
+       ;;(format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
@@ -65,12 +66,15 @@
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
+       (whitespace +guess +trim)  ; a butler for your whitespace
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
        dired             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
-       ;;ibuffer         ; interactive buffer management
+       ;;eww               ; the internet is gross
+       ;;ibuffer           ; interactive buffer management
+       tramp             ; remote files at your arthritic fingertips
        undo              ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
@@ -78,7 +82,7 @@
        ;;eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       vterm             ; the best terminal emulation in Emacs
+       ;;vterm             ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
@@ -91,22 +95,20 @@
        ;;collab            ; buffers with friends
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       docker
-       ;;editorconfig      ; let someone else argue about tabs vs spaces
+       ;;docker
+       editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        lookup              ; navigate your code and its documentation
-       lsp               ; M-x vscode
+       ;;llm               ; when I said you needed friends, I didn't mean...
+       ;;(lsp +eglot)      ; M-x vscode
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
-       ;;prodigy           ; FIXME managing external services & code builders
-       ;;rgb               ; creating color strings
-       ;;taskrunner        ; taskrunner for all your projects
-       ;;terraform         ; infrastructure as code
+       (terraform +tree-sitter)        ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
-       ;;tree-sitter       ; syntax and parsing, sitting in a tree...
+       tree-sitter       ; syntax and parsing, sitting in a tree...
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
@@ -114,6 +116,7 @@
        ;;tty               ; improve the terminal Emacs experience
 
        :lang
+       ;;ada               ; In strong typing we (blindly) trust
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
        ;;(cc +lsp)         ; C > C++ == 1
@@ -136,36 +139,42 @@
        ;;fsharp            ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
-       (go +lsp)         ; the hipster dialect
+       ;;(go +lsp)         ; the hipster dialect
        ;;(graphql +lsp)    ; Give queries a REST
        ;;(haskell +lsp)    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
-       json              ; At least it ain't XML
+       ;;json              ; At least it ain't XML
+       (json +tree-sitter) ; At least it ain't XML
+       ;;janet             ; Fun fact: Janet is me!
        ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
-       (javascript +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript +tree-sitter)        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        ;;lua               ; one-based indices? one-based indices
-       markdown          ; writing docs for people to ignore
+       (markdown +tree-sitter)         ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +roam2)         ; organize your plain life in plain text
-       php               ; perl's insecure younger brother
+       org               ; organize your plain life in plain text
+       ;;php               ; perl's insecure younger brother
+       (php +tree-sitter)               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
+       ;;graphviz          ; diagrams for confusing yourself even more
        ;;purescript        ; javascript, but functional
-       python            ; beautiful is better than ugly
+       ;;python            ; beautiful is better than ugly
+       (python +tree-sitter)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       (rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (rust +tree-sitter)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
@@ -173,8 +182,9 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       (web +lsp)               ; the tubes
-       yaml              ; JSON, but readable
+       ;;web               ; the tubes
+       (web +tree-sitter)               ; the tubes
+       (yaml +tree-sitter)              ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
@@ -188,7 +198,6 @@
        ;;everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
-       ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
        ;;literate
